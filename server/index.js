9a85cc3c2eaf4d28003c7ecf3d3a581b52bcefd6
Oluwaseun Cardoso <oluwaseun.cardosos@mail.utoronto.ca>
2023-03-30 15:22:18 -0400

    added node_moudles to git ignore

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 'use strict';
 // you might use a socket https://socket.io/docs/v4/
+var cors = require('cors')
+
 
 const 
 express = require('express'),
 app = express();
-var port = 3000
+
+app.use(cors());
+var port = 8080
 app.get("/", function(req, res){
     res.send("Hello, world!");
 });
